95c2c47928dc7ad5df5b8948ae628d8d99a68f62
AgamPandey133 <pandeyagam03@gmail.com>
2026-05-02 11:37:30 +0530

    prepare for render deploy

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { Link, useLocation } from "react-router";
 import useAuthUser from "../hooks/useAuthUser";
-import { BellIcon, HomeIcon, ShipWheelIcon, UsersIcon } from "lucide-react";
+import { BellIcon, HomeIcon, ShipWheelIcon, UsersIcon, UserCheckIcon } from "lucide-react";
 import AvatarImage from "./AvatarImage";
 
 const Sidebar = () => {
@@ -38,6 +38,16 @@ const Sidebar = () => {
         >
           <UsersIcon className="size-5 text-base-content opacity-70" />
           <span>Friends</span>
+        </Link>
+
+        <Link
+          to="/mentors"
+          className={`btn btn-ghost justify-start w-full gap-3 px-3 normal-case ${
+            currentPath === "/mentors" ? "btn-active" : ""
+          }`}
+        >
+          <UserCheckIcon className="size-5 text-base-content opacity-70" />
+          <span>Find Mentors</span>
         </Link>
 
         <Link
